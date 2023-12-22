3bdd20b233b3329f2c2f92b98434172d9ff74fe1
zjhczl <zhoujie.hczl@gmail.com>
2023-12-22 17:48:41 +0800

    s

--- test.py
+++ test.py
@@ -1,19 +1,18 @@
 from pyproj import CRS, Transformer
 
 # 定义WGS84坐标系
-wgs84_crs = CRS("EPSG:4326")
+wgs84_crs = CRS('epsg:4326')
 
-# 定义CGCS2000坐标系
-cgcs2000_crs = CRS("EPSG:4490")
+# 定义横轴墨卡托投影坐标系
+tmerc_crs = CRS('+proj=tmerc +lat_0=35.0773230869776 +lon_0=117.3635622350329 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +geoidgrids=egm96_15.gtx +vunits=m +no_defs')
 
-# 创建转换器
-transformer = Transformer.from_crs(wgs84_crs, cgcs2000_crs)
+# 创建一个从WGS84到横轴墨卡托投影坐标系的转换器
+transformer = Transformer.from_crs(wgs84_crs, tmerc_crs)
 
-# 示例WGS84坐标 (经度, 纬度)
-longitude, latitude = 117.20115, 39.12356
+# 假设有一个WGS84坐标点
+lon, lat = 117.0, 35.0
 
-# 转换坐标
-cgcs2000_longitude, cgcs2000_latitude = transformer.transform(
-    latitude, longitude)
+# 将WGS84坐标转换为横轴墨卡托投影坐标
+x, y = transformer.transform(lat, lon)
 
-print(f"CGCS2000 Coordinates: {cgcs2000_longitude}, {cgcs2000_latitude}")
+print(f"Projected coordinates: x={x}, y={y}")
